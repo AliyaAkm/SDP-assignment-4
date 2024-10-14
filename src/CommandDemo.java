0ead646228a5460ae7ff6b2b6151bda72128b1c8
Your Name <you@example.com>
2024-10-14 18:44:08 +0500

    sdp 4 assignment

---
+++ src/CommandDemo.java
@@ -0,0 +1,18 @@
+// Task 2
+public class CommandDemo {
+    public static void main(String[] args) {
+
+        Light livingRoomLight = new Light();
+
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
---
+++ src/NameRepository.java
@@ -0,0 +1,24 @@
+class NameRepository {
+    private String[] names = {"Aliya", "Anel", "Bexultan", "Yerkezhan"};
+
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            return index < names.length;
+        }
+
+        @Override
+        public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
